a090495ee171e6b440a563e1b8db823f1b7e6071
danzoniel <danigaara.ds@gmail.com>
2024-04-08 22:17:32 -0300

    feat: sjf almost finished

--- app/algorithms/sjf.go
+++ app/algorithms/sjf.go
@@ -13,73 +13,81 @@ func (s *Sjf) ShortestJobFirst() []Process {
 	fmt.Println("\nSHORTEST JOB FIRST")
 
 	p := make([]Process, len(s.Processes))
-	availabe := make([]Process, 0)
+	available := make([]Process, 0)
 	shortestJob := Process{}
 
 	actualInstant := 0
 
+	//Cria um novo array com todos os processos
 	for i := range s.Processes {
 		p[i] = s.Processes[i].NewProcess()
 	}
 
-	for i := range s.Processes {
-		if p[i].ArrivedTime <= actualInstant {
-			availabe = append(availabe, p[i])
-		}
-	}
-	index := 0
-	for y := 0; y <= len(availabe)-1; y++ {
-		if availabe[0].ArrivedTime == availabe[y].ArrivedTime {
-			if availabe[0].ServiceTime > availabe[y].ServiceTime {
-				shortestJob = availabe[y]
-				index = y
+	n := 0
+	for n <= 4 {	
+
+			//Cria um novo array com os processos disponíveis para o instante atual
+			waitingProcessess := make([]Process, 0)
+			for i := range p {
+				if p[i].ArrivedTime <= actualInstant {
+					available = append(available, p[i])
+				} else {
+					waitingProcessess = append(waitingProcessess, p[i])
+				}
 			}
+
+			p = nil
+			p = waitingProcessess
+
+			fmt.Println("Processos disponíveis para executar", available)
+			fmt.Println("Processos na lista de espera", p)
+
+			//Procura o menor tempo entre os disponíveis
+			index := 0
+			fmt.Println("Valor do shortest job arrived time: ", shortestJob.ArrivedTime)
+			for i, job := range available {
+				if job.ServiceTime < shortestJob.ServiceTime || i == 0{
+					index = i
+					shortestJob = job
+				}
+			}
+
+			fmt.Println("Processo com o menor tempo de serviço que será executado: ", shortestJob)
+
+			fmt.Println("Index do processo que será removido da lista de disponíveis: ", index)
+
+			fmt.Println("Instante atual antes do processo: ", actualInstant)
+
+			//Encontra o instante de término do processo 
+			shortestJob.ProcessTime.finishedExecutingAt = actualInstant + shortestJob.ServiceTime
+
+			//Incrementa o instante atual do algoritmo
+			actualInstant = shortestJob.ProcessTime.finishedExecutingAt
+
+			fmt.Println("Instante que o processo atual terminou de executar: ", shortestJob.ProcessTime.finishedExecutingAt)
+			fmt.Println("Instante atual: ", actualInstant)
+
+			//Limpa o processo
+			shortestJob = Process{}
+
+			//Remove o processo da lista de processos disponíveis
+			available = append(available[:index], available[index+1:]...)
+			fmt.Println("Lista de disponíveis depois da remoção: ", available)
+
+			n++
 		}
+
+		return nil
 	}
 
-	shortestJob.ProcessTime.finishedExecutingAt = actualInstant + shortestJob.ServiceTime
+// func removeMatchedElements(slice []Process, match int) []int {
+// 	result := []int{}
 
-	actualInstant = shortestJob.ProcessTime.finishedExecutingAt
+// 	for _, element := range slice {
+// 		if element != match {
+// 			result = append(result, element)
+// 		}
+// 	}
 
-	fmt.Println(availabe)
-	fmt.Println(shortestJob)
-
-	shortestJob = Process{}
-	availabe = append(availabe[:index], availabe[index+1:]...)
-
-	fmt.Println(availabe)
-	fmt.Println(shortestJob)
-	fmt.Println(actualInstant)
-
-	// for i := range s.Processes {
-
-	// 	if p[i].ArrivedTime <= actualInstant {
-	// 		availabe = append(availabe, p[i])
-	// 	}
-	// }
-
-	// for y := 0; y <= len(availabe)-1; y++ {
-	// 	if availabe[0].ArrivedTime == availabe[y].ArrivedTime {
-	// 		if availabe[0].ServiceTime > availabe[y].ServiceTime {
-	// 			shortestJob = append(shortestJob, availabe[y])
-	// 		}
-	// 	}
-	// }
-
-	// fmt.Println(availabe)
-	// fmt.Println(shortestJob)
-
-	return nil
-}
-
-func removeMatchedElements(slice []Process, match int) []int {
-	result := []int{}
-
-	for _, element := range slice {
-		if element != match {
-			result = append(result, element)
-		}
-	}
-
-	return result
-}
+// 	return result
+// }
